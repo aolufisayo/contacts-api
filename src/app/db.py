953a7528e46b5d11bee6e9ee6b48e9c436b0a4ff
Olufisayo Akinmoju <phissycodes@hotmail.com>
2020-09-06 20:04:15 +0100

    fastapi app setup

---
+++ src/app/db.py
@@ -0,0 +1,21 @@
+import os
+from databases import Database
+from sqlalchemy import (Column, DateTime, Integer, String, Table, create_engine, MetaData)
+from sqlalchemy.sql import func
+
+DATABASE_URL = os.getenv("database_url")
+
+#SQLALCHEMY
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+contacts = Table(
+    "contacts",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("first_name", String(50)),
+    Column("last_ame", String(50)),
+    Column("created_date", DateTime, default=func.now(), nullable=False),
+)
+
+#database query builder
+database = Database(DATABASE_URL)
---
+++ src/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.db import database, engine, metadata
+
+metadata.create_all(engine)
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
